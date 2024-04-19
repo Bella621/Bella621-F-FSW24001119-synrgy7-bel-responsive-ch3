2073bfc89e00a5a716683c66bd6e497bd60a486c
Bella621 <92489927+Bella621@users.noreply.github.com>
2024-04-19 22:02:13 +0700

    uplod ulang

--- scripts/sortCarByYearAscendingly.js
+++ scripts/sortCarByYearAscendingly.js
@@ -3,15 +3,21 @@ function sortCarByYearAscendingly(cars) {
   console.log(cars);
 
   // Clone array untuk menghindari side-effect
-  // Apa itu side effect?
   const result = [...cars];
 
+  // Implementasi algoritma Bubble Sort
+  for (let i = 0; i < result.length - 1; i++) {
+    for (let j = 0; j < result.length - 1 - i; j++) {
+      if (result[j].year > result[j + 1].year) {
+        // Swap
+        const temp = result[j];
+        result[j] = result[j + 1];
+        result[j + 1] = temp;
+      }
+    }
+  }
 
-  // Sorting array berdasarkan tahun secara ascending
-  result.sort((a, b) => a.year - b.year);
-  
-  console.log("sortir data ascending" ,result)
-
+  console.log("sortir data ascending", result);
   // Rubah code ini dengan array hasil sorting secara ascending
   return result;
 }
--- scripts/sortCarByYearDecendingly.js
+++ scripts/sortCarByYearDecendingly.js
@@ -1,14 +1,24 @@
 function sortCarByYearDescendingly(cars) {
   // Sangat dianjurkan untuk console.log semua hal hehe
   console.log(cars);
-
-  // Clone array untuk menghindari side-effect
-  // Apa itu side effect?
-  const result = [...cars];
   
-  result.sort((a, b) => b.year - a.year);
-  // Tulis code-mu disini
-  console.log("data sortir descending" , result)
+// Clone array untuk menghindari side-effect
+const result = [...cars];
+  
+// Implementasi algoritma Bubble Sort
+for (let i = 0; i < result.length - 1; i++) {
+  for (let j = 0; j < result.length - 1 - i; j++) {
+    if (result[j].year < result[j + 1].year) {
+      // Swap
+      const temp = result[j];
+      result[j] = result[j + 1];
+      result[j + 1] = temp;
+    }
+  }
+}
+
+// Tulis code-mu disini
+console.log("data sortir descending", result)
 
   // Rubah code ini dengan array hasil sorting secara descending
   return result;
